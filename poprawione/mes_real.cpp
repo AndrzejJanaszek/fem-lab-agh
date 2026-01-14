6e7926342a550438bd87734a2072b5c1e4b4378f
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2026-01-14 20:37:10 +0100

    poprawa dyfuzji - uwzglednia wielkosc elementow i sasiedztwo miedzi

--- poprawione/mes_real.cpp
+++ poprawione/mes_real.cpp
@@ -1,4 +1,18 @@
 // #############################################
+//* ############### ABOUT ###############
+// #############################################
+
+// Andrzej Janaszek
+// Informatyka Techniczna 3 rok
+// MES
+//
+// założenia programu:
+// element 4 węzłowy
+// liczba możliwych punktów całkowania: 2,3,4
+//
+// stan kodu: It's a MESs
+
+// #############################################
 //* ############### IMPORTS ###############
 // #############################################
 #include <iostream>
@@ -37,7 +51,7 @@ const double AIR_SPECIFIC_HEAT = 1004.0;
 
  // W/m3
  // 13mm*13mm*1.3mm / 1000^3 --- mm3 => m3
-const double HEAT_GENERATION = 133 / (13*13*1.3 / (1000*1000*1000));
+const double HEAT_GENERATION = 2.3 / (13*13*1.3 / (1000*1000*1000));
 
 const double SCALE_FACTOR_X = 0.39;
 // const double SCALE_FACTOR_X = 1;
@@ -45,24 +59,27 @@ const double SCALE_FACTOR_Y = SCALE_FACTOR_X;
 
 const double MAX_TEMP_LIMIT = 100;
 
+//* CPU DIE, IHS, RADIATOR - (COPPER)
+// 4 kolumny - zebra
+const std::vector<int> COPPER_ELEMENTS_4 = {95,96,97,101,102,103,107,108,109,113,114,115,125,126,127,131,132,133,137,138,139,143,144,145,155,156,157,161,162,163,167,168,169,173,174,175,185,186,187,191,192,193,197,198,199,203,204,205,215,216,217,221,222,223,227,228,229,233,234,235,245,246,247,251,252,253,257,258,259,263,264,265,275,276,277,281,282,283,287,288,289,293,294,295,305,306,307,311,312,313,317,318,319,323,324,325,335,336,337,341,342,343,347,348,349,353,354,355,365,366,367,371,372,373,377,378,379,383,384,385,395,396,397,401,402,403,407,408,409,413,414,415,425,426,427,431,432,433,437,438,439,443,444,445,455,456,457,461,462,463,467,468,469,473,474,475,485,486,487,491,492,493,497,498,499,503,504,505,515,516,517,521,522,523,527,528,529,533,534,535,545,546,547,551,552,553,557,558,559,563,564,565,575,576,577,581,582,583,587,588,589,593,594,595,605,606,607,611,612,613,617,618,619,623,624,625,635,636,637,641,642,643,647,648,649,653,654,655,665,666,667,671,672,673,677,678,679,683,684,685,695,696,697,701,702,703,707,708,709,713,714,715,725,726,727,731,732,733,737,738,739,743,744,745,755,756,757,761,762,763,767,768,769,773,774,775,785,786,787,791,792,793,797,798,799,803,804,805,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900};
+
+// 2 kolumny - zebra
+const std::vector<int> COPPER_ELEMENTS_2 = {95,96,97,98,99,100,110,111,112,113,114,115,125,126,127,128,129,130,140,141,142,143,144,145,155,156,157,158,159,160,170,171,172,173,174,175,185,186,187,188,189,190,200,201,202,203,204,205,215,216,217,218,219,220,230,231,232,233,234,235,245,246,247,248,249,250,260,261,262,263,264,265,275,276,277,278,279,280,290,291,292,293,294,295,305,306,307,308,309,310,320,321,322,323,324,325,335,336,337,338,339,340,350,351,352,353,354,355,365,366,367,368,369,370,380,381,382,383,384,385,395,396,397,398,399,400,410,411,412,413,414,415,425,426,427,428,429,430,440,441,442,443,444,445,455,456,457,458,459,460,470,471,472,473,474,475,485,486,487,488,489,490,500,501,502,503,504,505,515,516,517,518,519,520,530,531,532,533,534,535,545,546,547,548,549,550,560,561,562,563,564,565,575,576,577,578,579,580,590,591,592,593,594,595,605,606,607,608,609,610,620,621,622,623,624,625,635,636,637,638,639,640,650,651,652,653,654,655,665,666,667,668,669,670,680,681,682,683,684,685,695,696,697,698,699,700,710,711,712,713,714,715,725,726,727,728,729,730,740,741,742,743,744,745,755,756,757,758,759,760,770,771,772,773,774,775,785,786,787,788,789,790,800,801,802,803,804,805,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900};
+
+// 1 kolumna - zebro
+const std::vector<int> COPPER_ELEMENTS_1 = {95,96,97,98,99,100,110,111,112,113,114,115,125,126,127,128,129,130,140,141,142,143,144,145,155,156,157,158,159,160,170,171,172,173,174,175,185,186,187,188,189,190,200,201,202,203,204,205,215,216,217,218,219,220,230,231,232,233,234,235,245,246,247,248,249,250,260,261,262,263,264,265,275,276,277,278,279,280,290,291,292,293,294,295,305,306,307,308,309,310,320,321,322,323,324,325,335,336,337,338,339,340,350,351,352,353,354,355,365,366,367,368,369,370,380,381,382,383,384,385,395,396,397,398,399,400,410,411,412,413,414,415,425,426,427,428,429,430,440,441,442,443,444,445,455,456,457,458,459,460,470,471,472,473,474,475,485,486,487,488,489,490,500,501,502,503,504,505,515,516,517,518,519,520,530,531,532,533,534,535,545,546,547,548,549,550,560,561,562,563,564,565,575,576,577,578,579,580,590,591,592,593,594,595,605,606,607,608,609,610,620,621,622,623,624,625,635,636,637,638,639,640,650,651,652,653,654,655,665,666,667,668,669,670,680,681,682,683,684,685,695,696,697,698,699,700,710,711,712,713,714,715,725,726,727,728,729,730,740,741,742,743,744,745,755,756,757,758,759,760,770,771,772,773,774,775,785,786,787,788,789,790,800,801,802,803,804,805,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900};
+
+//* CPU DIE - CZESC PROCESORA, POLPRZEWODNIK
+const std::vector<int> INITIAL_HOT_ELEMENTS = {881,882,883,884,885,886,887,888,889,890};
+
 // #############################################
 //* ############### MESH RELATIVE DATA ###############
 // #############################################
 
-// IHS I RESZTA RADIATORA
-std::vector<int> COPPER_ELEMENTS = {95,96,97,101,102,103,107,108,109,113,114,115,125,126,127,131,132,133,137,138,139,143,144,145,155,156,157,161,162,163,167,168,169,173,174,175,185,186,187,191,192,193,197,198,199,203,204,205,215,216,217,221,222,223,227,228,229,233,234,235,245,246,247,251,252,253,257,258,259,263,264,265,275,276,277,281,282,283,287,288,289,293,294,295,305,306,307,311,312,313,317,318,319,323,324,325,335,336,337,341,342,343,347,348,349,353,354,355,365,366,367,371,372,373,377,378,379,383,384,385,395,396,397,401,402,403,407,408,409,413,414,415,425,426,427,431,432,433,437,438,439,443,444,445,455,456,457,461,462,463,467,468,469,473,474,475,485,486,487,491,492,493,497,498,499,503,504,505,515,516,517,521,522,523,527,528,529,533,534,535,545,546,547,551,552,553,557,558,559,563,564,565,575,576,577,581,582,583,587,588,589,593,594,595,605,606,607,611,612,613,617,618,619,623,624,625,635,636,637,641,642,643,647,648,649,653,654,655,665,666,667,671,672,673,677,678,679,683,684,685,695,696,697,701,702,703,707,708,709,713,714,715,725,726,727,731,732,733,737,738,739,743,744,745,755,756,757,761,762,763,767,768,769,773,774,775,785,786,787,791,792,793,797,798,799,803,804,805,811,812,813,814,815,816,817,818,819,820,821,822,823,824,825,826,827,828,829,830,831,832,833,834,835,836,837,838,839,840,841,842,843,844,845,846,847,848,849,850,851,852,853,854,855,856,857,858,859,860,861,862,863,864,865,866,867,868,869,870,871,872,873,874,875,876,877,878,879,880,881,882,883,884,885,886,887,888,889,890,891,892,893,894,895,896,897,898,899,900};
-// std::vector<int> COPPER_ELEMENTS = {1
-// };
+//* USTAWIENIE ILOSCI KOLUMN
+std::vector<int> COPPER_ELEMENTS = COPPER_ELEMENTS_4;
 
-// CPU DIE - CZESC PROCESORA, POLPRZEWODNIK
-std::vector<int> INITIAL_HOT_ELEMENTS = {881,882,883,884,885,886,887,888,889,890};
-// std::vector<int> INITIAL_HOT_ELEMENTS = {
-// 404,405,406,434,435,436,464,465,466
-// };
 
-// założenia programu:
-// element 4 węzłowy
-// liczba możliwych punktów całkowania: 2,3,4
 
 // #############################################
 //* ############### FUNCTIONS ###############
@@ -350,6 +367,7 @@ void agregate_time_part(Grid &grid, GlobalData& globalData, EquationData& eqData
     }
 }
 
+
 void print_H_from_elements(Grid& grid){
     std::vector<std::vector<double>> H;
     H.assign(grid.node_number, std::vector<double>(grid.node_number, 0.0));
@@ -410,7 +428,6 @@ void print_P_from_elements(Grid& grid){
 }
 
 
-
 void print_H(EquationData& ed){
     printf("H:\n");
     for(auto& row : ed.H){
@@ -438,6 +455,7 @@ void print_P(EquationData& ed){
     }
     printf("\n");
 }
+
 
 void scale_mesh(Grid& grid, double scale_factor_x, double scale_factor_y){
     for(auto& node : grid.nodes){
@@ -486,39 +504,82 @@ TempInfo print_temp_info(const std::vector<double>& temp_vec, double min_temp_co
 }
 
 // ustawia wektor elementów powietrza
-void set_air_elemnts_ids(std::vector<int>& air_elemnts_ids, Grid& grid) {
-    air_elemnts_ids.clear();
+void set_air_elements_ids(std::vector<int>& air_elements_ids, Grid& grid) {
+    air_elements_ids.clear();
 
-    std::unordered_set<int> copper_set(COPPER_ELEMENTS.begin(), COPPER_ELEMENTS.end());
+    // ELEMENTY MIEDZI
+    std::unordered_set<int> copper_elements(
+        COPPER_ELEMENTS.begin(), COPPER_ELEMENTS.end()
+    );
 
-    for (int id = 1; id <= grid.element_number; ++id) {
-        if (copper_set.find(id) == copper_set.end()) {
-            air_elemnts_ids.push_back(id);
+    // WEZLY MIEDZIANE
+    std::unordered_set<int> copper_nodes;
+
+    for (int elem_id : COPPER_ELEMENTS) {
+        const Element4& el = grid.elements[elem_id - 1];
+        for (int nid : el.node_ids) {
+            copper_nodes.insert(nid);
+        }
+    }
+
+    // DLA KAZDEGO ELEMENTU SPRAWDZAMY CZY JEST POWIETRZEM I CZY NIE SASIADUJE Z MIEDZIA
+    for (int id = 1; id <= grid.element_number; ++id)
+    {
+        // JEZELI MIEDZ SKIP
+        if (copper_elements.count(id))
+            continue;
+
+        const Element4& el = grid.elements[id - 1];
+
+        // SPRAWDZENIE CZY MIEDZ JEST SASIADEM
+        bool collides = false;
+        for (int nid : el.node_ids) {
+            if (copper_nodes.count(nid)) {
+                collides = true;
+                break;
+            }
+        }
+
+        // JAK BRAK SIASADUJACEJ MIEDZI DODAJ DO POWIETRZA
+        if (!collides) {
+            air_elements_ids.push_back(id);
         }
     }
 }
 
-// uśrednia wartość temperatury dla całęgo powietrza
+// uśrednia wartość temperatury dla całęgo powietrza (srednia wazona polem/jakobianami elementow)
 void air_diffusion(std::vector<double>& temp_vec, const std::vector<int>& air_elemnts_ids, Grid& grid){
     // jezeli brak powietrza zakoncz
     if(air_elemnts_ids.empty()) return;
 
     std::unordered_set<int> air_nodes_ids;
-    double sum = 0;
+
+    double sum = 0;             // suma temperatur
+    double total_detJ = 0;      // suma jakobianow
+
     // dla każdego elementu powietrza
     for(auto& el : air_elemnts_ids){
+        auto& el_obj = grid.elements[el-1];
 
         // policz srednia temperature w elemencie powietrza (suma temp w nodach / 4)
-        // i dodaj do ogólnej sumy
+        // uwzglednij jego jakobian ("pole") i dodaj do sumy
+        // sumuj jakobiany
+
         double el_sum = 0;
         for(auto& node_id : grid.elements[el-1].node_ids){
             el_sum += temp_vec[node_id-1];
             air_nodes_ids.insert(node_id);
         }
-        sum += el_sum/4;
+        // suma wyznacznikow dla kazdego punktu calkowania -> odpowiednik pola elementu
+        double el_detJ = std::accumulate(el_obj.jacobian.detJ.begin(), el_obj.jacobian.detJ.end(), 0.0);
+
+        // avg temp * "pole" elementu
+        sum += (el_sum/4) * el_detJ;
+        total_detJ += el_detJ;
     }
-    // srednia
-    sum = sum / air_elemnts_ids.size();
+
+    // srednia ważona "powierzchnią"(jakobianami) elementow
+    sum = sum / total_detJ;
 
     // przypisz średnią temperature
     for(auto& id : air_nodes_ids){
@@ -549,13 +610,13 @@ int main(int argc, char const *argv[])
     // ###############################################################
     //*#                  CONSTANTS AND VARIABLES
     // ###############################################################
-    std::vector<int> air_elemnts_ids;
+    std::vector<int> air_elements_ids;
     const int GAUSS_I_POINTS = 2;
     auto GAUSS_POINTS_ARRAY = GaussQuad::points_2;
-    double prev_max_temp;   // dla sprawdzenia czy proces jest ustalony
+    TempInfo prev_temp_info;   // dla sprawdzenia czy proces jest ustalony
 
-    const bool DIFFUSION = false;
-    const bool TIME_PART_AGREGATION = false;
+    const bool DIFFUSION = true;
+    const bool TIME_PART_AGREGATION = true;
 
     // printf("HTEAT: %lf\n", HEAT_GENERATION);
     // std::string RESULT_PATH = "results/1_4_4/";
@@ -580,8 +641,11 @@ int main(int argc, char const *argv[])
     EquationData equationData;
     load_data_from_file(data_file_path, global_data, grid);
     scale_mesh(grid, SCALE_FACTOR_X, SCALE_FACTOR_Y);
-    set_air_elemnts_ids(air_elemnts_ids, grid);
-    prev_max_temp = global_data.tot;
+    set_air_elements_ids(air_elements_ids, grid);
+
+    prev_temp_info.max = global_data.tot;
+    prev_temp_info.min = global_data.tot;
+    prev_temp_info.avg = global_data.tot;
 
     // for(auto& el : air_elemnts_ids){
     //     printf("%d, ", el);
@@ -596,7 +660,7 @@ int main(int argc, char const *argv[])
     // ###############################################################
     //*#                       SIMULATION
     // ###############################################################
-    
+
     //* SETUP INITIAL TEMPERATURE
     // std::vector<double> temperature_v_initial = std::vector<double>(global_data.node_number,global_data.initial_temperature);
     std::vector<double> temperature_v_initial = std::vector<double>(global_data.node_number, global_data.tot);
@@ -653,9 +717,14 @@ int main(int argc, char const *argv[])
 
         //* DYFUZJA DLA POWIETRZA
         if(DIFFUSION)
-            air_diffusion(temperature_v, air_elemnts_ids, grid);
+            air_diffusion(temperature_v, air_elements_ids, grid);
 
         TempInfo temp_info = print_temp_info(temperature_v, global_data.tot);
+        printf("grad || min: %lf max: %lf avg: %lf\n", 
+            temp_info.min - prev_temp_info.min, 
+            temp_info.max - prev_temp_info.max, 
+            temp_info.avg - prev_temp_info.avg
+        );
 
         
         
@@ -688,11 +757,11 @@ int main(int argc, char const *argv[])
         }
 
         //* STATIONARY BREAK
-        if(is_stationary(temperature_v, prev_max_temp, 0.01)){
+        if(is_stationary(temperature_v, prev_temp_info.max, 0.01)){
             printf("PROCES JEST USTALONY\nmax temp: %lf\n", temp_info.max);
             break;
         }
-        prev_max_temp = temp_info.max;
+        prev_temp_info = temp_info;
     }
 
     //* ZAPIS SYMULACJI DO PLIKU - OPIS KROKÓW SYMULACJI
